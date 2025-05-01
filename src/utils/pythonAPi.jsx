c0ec2b1edfd64be1390dd83fad10f0fb852a12eb
Indunil W W A D A <101281237+Anjana2k02@users.noreply.github.com>
2025-05-01 15:47:27 +0530

    setup fastAPI python port 8000

---
+++ src/utils/pythonAPi.jsx
@@ -0,0 +1,19 @@
+// Add FastAPI base URL
+const HOST_FASTAPI = "http://localhost:8000";
+
+// Create a separate instance if needed
+const axiosFastApi = axios.create({ baseURL: HOST_FASTAPI });
+
+axiosFastApi.interceptors.response.use(
+  (res) => res,
+  (error) => Promise.reject(error.response || 'Something went wrong')
+);
+
+// New fetcher for FastAPI GET with params
+export const getFastAPIParamFetcher = async (url, params = {}, config = {}) => {
+  const res = await axiosFastApi.get(url, {
+    params,
+    ...config,
+  });
+  return res.data;
+};
