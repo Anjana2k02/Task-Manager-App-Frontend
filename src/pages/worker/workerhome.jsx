2050e51d7ffdd0aab7bd711c144ee8efe4c9cfd7
Indunil W W A D A <101281237+Anjana2k02@users.noreply.github.com>
2025-05-11 02:38:51 +0530

    conflict resolved

--- src/pages/worker/workerhome.jsx
+++ src/pages/worker/workerhome.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Delete } from '@mui/icons-material';
 import {
   AppBar, Avatar, Box, Button, Container, Grid, IconButton, InputAdornment,
   Paper, TextField, Toolbar, Typography, Autocomplete, CircularProgress, Divider
